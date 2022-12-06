7d32d0d2b172719f5236216243c4c5940819b494
Harsh Patel <harshp2015m@gmail.com>
2022-12-06 00:01:46 -0500

    Added ISR SSG tweet pages

--- src/pages/thread/[id].tsx
+++ src/pages/thread/[id].tsx
@@ -1,38 +1,60 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router'
-import Footer from '../../components/Footer';
+import type { InferGetStaticPropsType } from 'next';
 import Layout from '../../components/Layout';
-import Navbar from '../../components/Navbar';
 import { Thread } from '../../components/Thread';
 import { trpc } from '../../utils/trpc';
 
-export default function ThreadPage() {
-    const router = useRouter()
-    const id = router.query.id as string
+export async function getStaticPaths() {
+    return { paths: [], fallback: true }
+}
+
+
+export const getStaticProps = async ({ params }: { params: string }) => {
+    const thread_id = params
+
+    if (thread_id.length > 40) {
+        return { notFound: true }
+    }
+
     const { data, isError, error } = trpc.threads.get_thread_by_id.useQuery(
-        { thread_id: id },
+        { thread_id: thread_id },
         {
             refetchOnReconnect: false,
             refetchOnWindowFocus: false
         }
     )
 
+    if (!data) {
+        return { notFound: true }
+    }
+
     if (isError) {
-        console.log(error.message);
+        console.log(error.message)
+        return { notFound: true }
     }
-    if (data?.thread) {
-        return (
-            <>
-                <Head>
-                    <title>Threads</title>
-                    <meta name="description" content="Thread" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                <Layout>
-                    <Thread thread={data.thread} numExpanded={data.thread.tweet.length} />
-                </Layout>
-            </>
-        )
+    return {
+        props: {
+            thread: data?.thread
+        }
     }
-    return <div className="thread-loading">Loading...</div>
+
+}
+
+
+export default function ThreadPage({ thread }: InferGetStaticPropsType<typeof getStaticProps>) {
+    if (!thread) {
+        return <h1>Not Found</h1>
+    }
+    return (
+        <>
+            <Head>
+                <title>Threads</title>
+                <meta name="description" content="Thread" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Layout>
+                <Thread thread={thread} numExpanded={thread.tweet.length} />
+            </Layout>
+        </>
+    )
 }
