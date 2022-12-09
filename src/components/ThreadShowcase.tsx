69c7717cb1ad1804b5f62dc806215bac55e3900f
Harsh Patel <harshp2015m@gmail.com>
2022-12-09 17:11:22 -0500

    Removed null check on threads in showcase

--- src/components/ThreadShowcase.tsx
+++ src/components/ThreadShowcase.tsx
@@ -1,28 +1,27 @@
 import type { ThreadType } from "./Thread";
 import { Thread } from "./Thread";
-import { trpc } from "../utils/trpc";
 
 
 
 export function ThreadShowcase({ threads }: { threads: ThreadType[] }): JSX.Element {
 
-    if (threads) {
-        return (
-            <div className="showcase flex justify-center">
-                <div className="xl:columns-2 3xl:columns-3 gap-2 p-5">
-                    {
-                        threads.map((thread, index) => {
-                            // randomly choose number of expanded tweets
-                            let numExpanded = Math.floor(Math.random() * (4)) + 1;
-                            if (index % 3 == 0) {
-                                numExpanded = Math.floor(Math.random() * (2)) + 1;
-                            }
 
-                            return <Thread thread={thread} numExpanded={numExpanded} key={thread.id.toString()} />
-                        })
-                    }
-                </div>
-            </div >
-        )
-    }
+    return (
+        <div className="showcase flex justify-center">
+            <div className="xl:columns-2 3xl:columns-3 gap-2 p-5">
+                {
+                    threads.map((thread, index) => {
+                        // randomly choose number of expanded tweets
+                        let numExpanded = Math.floor(Math.random() * (4)) + 1;
+                        if (index % 3 == 0) {
+                            numExpanded = Math.floor(Math.random() * (2)) + 1;
+                        }
+
+                        return <Thread thread={thread} numExpanded={numExpanded} key={thread.id.toString()} />
+                    })
+                }
+            </div>
+        </div >
+    )
+
 }
