553f09f9dbdf1f49addb6e88465b844c92ec9798
Harsh Patel <harshp2015m@gmail.com>
2022-12-18 04:23:21 -0500

    Added on demand ssg to author pages

--- src/pages/author/[id].tsx
+++ src/pages/author/[id].tsx
@@ -1,38 +1,58 @@
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router'
 import Layout from '../../components/Layout';
 import ThreadShowcase from '../../components/ThreadShowcase';
-import { trpc } from '../../utils/trpc';
+import { createContextInner } from '../../server/trpc/context';
+import { threadsRouter } from '../../server/trpc/router/threadsRouter';
+import superjson from 'superjson'
+import { createProxySSGHelpers } from '@trpc/react-query/ssg';
 
-export default function ThreadPage() {
-    const router = useRouter()
-    const id = router.query.id as string
-    const { data, isError, error } = trpc.threads.get_threads_by_author_id.useQuery(
-        { author_id: id },
-        {
-            refetchOnReconnect: false,
-            refetchOnWindowFocus: false
-        }
-    )
+export async function getStaticPaths() {
+    return { paths: [], fallback: true }
+}
 
-    if (isError) {
-        console.log(error.message);
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+    const ssg = createProxySSGHelpers({
+        router: threadsRouter,
+        ctx: await createContextInner(),
+        transformer: superjson
+    })
+    const id = params?.id as string
+    const data = await ssg.get_threads_by_author_id.fetch({ author_id: id })
+
+    return {
+        props: {
+            threads: data.threads
+        },
+        revalidate: 600
     }
-    if (data?.threads) {
+
+}
+export default function ThreadPage({ threads }: InferGetStaticPropsType<typeof getStaticPaths>) {
+    const router = useRouter()
+
+    if (router.isFallback) {
         return (
-            <>
-                <Head>
-                    <title>Threads</title>
-                    <meta name="description" content="Twitter threads" />
-                    <link rel="icon" href="/favicon.ico" />
-                    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-                </Head>
-                <Layout>
-                    <div className='mt-5'>
-                        <ThreadShowcase threads={data.threads} />
-                    </div>
-                </Layout>
-            </>
+            <Layout>
+                <div>Loadin threads by tha author...</div>
+            </Layout>
         )
     }
+
+    return (
+        <>
+            <Head>
+                <title>Threads</title>
+                <meta name="description" content="Twitter threads" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+            </Head>
+            <Layout>
+                <div className='mt-5'>
+                    <ThreadShowcase threads={threads} />
+                </div>
+            </Layout>
+        </>
+    )
 }
