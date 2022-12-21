05486242a609975e69d1052048cf24b6c09d7f3c
Harsh Patel <harshp2015m@gmail.com>
2022-12-20 22:07:57 -0500

    Added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
+import { Analytics } from '@vercel/analytics/react';
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
@@ -13,6 +13,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 };
