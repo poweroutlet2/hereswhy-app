ff39ce7761249089a5bf514dbe6473b6110820e3
Harsh Patel <harshp2015m@gmail.com>
2022-12-23 21:45:30 -0500

    Created searchbar and placed it on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import testProfilePic from '../../public/dog.jpg'
 import threadLogo from '../../public/thread_logo.png'
 import Link from 'next/link'
+import Searchbar from './Searchbar'
 
 const navigation = [
     { name: 'Home', href: '/', current: true },
@@ -67,13 +68,7 @@ export default function Navbar() {
                                 </div>
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                                <button
-                                    type="button"
-                                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                >
-                                    <span className="sr-only">View notifications</span>
-                                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                                </button>
+                                <Searchbar />
 
                                 {/* Profile dropdown */}
                                 <Menu as="div" className="relative ml-3">
