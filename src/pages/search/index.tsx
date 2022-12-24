615648272fee8880ab5d46e541633efd1b6a01d2
Harsh Patel <harshp2015m@gmail.com>
2022-12-23 21:45:46 -0500

    Added search page

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,45 @@
+import { createProxySSGHelpers } from "@trpc/react-query/ssg"
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from "next"
+import { createContextInner } from "../../server/trpc/context"
+import { threadsRouter } from "../../server/trpc/router/threadsRouter"
+import { trpc } from "../../utils/trpc"
+import superjson from 'superjson'
+import Layout from "../../components/Layout"
+import ThreadShowcase from "../../components/ThreadShowcase"
+import Head from "next/head"
+
+export async function getServerSideProps({ query }: GetServerSidePropsContext) {
+    const term = query.term as string
+    const ssg = createProxySSGHelpers({
+        router: threadsRouter,
+        ctx: await createContextInner(),
+        transformer: superjson
+    })
+
+    const results = await ssg.search_threads.fetch({ term: term })
+
+    return {
+        props: {
+            term: term,
+            data: results,
+        },
+    }
+}
+
+export default function SearchPage({ term, data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+    return (
+        <>
+            <Head>
+                <title>Threads</title>
+                <meta name="description" content="Twitter threads" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+            </Head>
+            <Layout>
+                <h1 className="text-3xl mt-5">{data.results.length} threads found for the term: {`"${term}"`}</h1>
+                <ThreadShowcase threads={data.results} />
+            </Layout>
+        </>
+    )
+}
