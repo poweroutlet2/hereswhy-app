79601b69849df147e016af42bbc44108b46ba962
Harsh Patel <harshp2015m@gmail.com>
2022-12-31 22:53:27 -0500

    css changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
                             </div>
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
-                                    <Link href='/'>
+                                    <Link passHref href='/'>
                                         <Image
                                             className="block h-8 w-auto lg:hidden"
                                             src={threadLogo}
@@ -66,7 +66,7 @@ export default function Navbar() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <div className="inset-y-0 right-0 flex items-center pr-2 sm:static sm:ml-6 sm:pr-0">
                                 <div className="hidden sm:block">
                                     <Searchbar />
                                 </div>
