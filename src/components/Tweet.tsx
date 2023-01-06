49caa63f4d21f1c7b47e0c827d7b0a0c9048cea3
Harsh Patel <harshp2015m@gmail.com>
2023-01-06 17:45:36 -0500

    Added @ mention parsing in tweet text and removed public metric links

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -10,13 +10,17 @@ import MediaContainer from "./MediaContainer";
 dayjs.extend(relativeTime)
 
 function dateFormatter(dateString: string) {
+    // formats the tweeted at datetime
     const date = dayjs(dateString)
     if (date > dayjs().subtract(1, 'day'))
         return date.fromNow()
     return date.format('MM/DD/YYYY')
 }
 
+
 function numberFormatter(num: number | null | undefined): string {
+    // formats the like, repy, and retweet counts
+
     if (!num) {
         return ''
     }
@@ -40,6 +44,19 @@ function linkFormatter(text: string | null): string | null {
         ;
     return anchor
 }
+
+function renderAtMentions(text: string) {
+    return text.replace(/@([a-z\d_]+)/ig, '<a href="http://twitter.com/$1" class="mention">@$1</a>');
+}
+
+function renderTweetText(text: string) {
+    // renders links and at mentions from tweet text
+    let result = md().render(text)
+    result = renderAtMentions(result)
+
+    return result
+}
+
 
 export function Tweet(
     { tweet, author, replies, likes, retweets, last, media }:
@@ -79,12 +96,12 @@ export function Tweet(
                         {/* <div className='text-lg mb-3 mt-1 leading-normal'>
                             {tweet.content}
                         </div> */}
-                        <div className="tweet-content text-base md:text-lg" dangerouslySetInnerHTML={{ __html: md().render(tweet.content) }} />
+                        <div className="tweet-content text-base md:text-lg" dangerouslySetInnerHTML={{ __html: renderTweetText(tweet.content) }} />
                         {media?.length != 0 ? <MediaContainer media={media} /> : ''}
                         {/* Metric buttons - only show if replies exists (is first tweet) */}
                         {replies &&
                             <div className="flex flex-row justify-center mt-4 -mb-2 gap-8 text-sm font-medium tracking-wider md:flex">
-                                < a className="flex text-base group gap-1 items-center" href={`https://twitter.com/intent/tweet?in_reply_to=${tweet.id.toString()}`
+                                <div className="flex text-base group gap-1 items-center" href={`https://twitter.com/intent/tweet?in_reply_to=${tweet.id.toString()}`
                                 } target="_blank" rel="noopener noreferrer" >
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mb-1 fill-transparent stroke-white stroke-2 fill-blue-500 duration-200">
                                         <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
@@ -95,8 +112,8 @@ export function Tweet(
                                             <span className="hidden md:block">Replies</span>
                                         </span>
                                     </div>
-                                </a>
-                                <a className="flex text-base group gap-1 items-center" href={`https://twitter.com/intent/retweet?tweet_id=${tweet.id.toString()}`} target="_blank" rel="noopener noreferrer">
+                                </div>
+                                <div className="flex text-base group gap-1 items-center" href={`https://twitter.com/intent/retweet?tweet_id=${tweet.id.toString()}`} target="_blank" rel="noopener noreferrer">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mb-1 fill-current fill-white fill-green-400 stroke-green-400 duration-100">
                                         <g><path d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z" />
                                         </g>
@@ -107,8 +124,8 @@ export function Tweet(
                                             <span className="hidden md:block">Retweets</span>
                                         </span>
                                     </div>
-                                </a>
-                                <a className="flex text-base group gap-1 items-center" href={`https://twitter.com/intent/like?tweet_id=${tweet.id.toString()}`} target="_blank" rel="noopener noreferrer">
+                                </div>
+                                <div className="flex text-base group gap-1 items-center" href={`https://twitter.com/intent/like?tweet_id=${tweet.id.toString()}`} target="_blank" rel="noopener noreferrer">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mb-1 fill-transparent stroke-2 fill-red-500 stroke-white duration-100">
                                         <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
                                     </svg>
@@ -118,7 +135,7 @@ export function Tweet(
                                             <span className="hidden md:block">Likes</span>
                                         </span>
                                     </div>
-                                </a>
+                                </div>
                             </div>
                         }
                     </div>
