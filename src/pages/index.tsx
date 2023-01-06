fc3f22738049f115a9be53745e53f16811ceb230
Harsh Patel <harshp2015m@gmail.com>
2023-01-06 13:43:11 -0500

    debugging twitter login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { createContextInner } from "../server/trpc/context";
 import superjson from 'superjson';
 import ThreadShowcase from "../components/ThreadShowcase";
 import type { InferGetStaticPropsType } from "next";
+import { env } from "../env/server.mjs";
 
 
 export async function getStaticProps() {
@@ -15,17 +16,23 @@ export async function getStaticProps() {
 
   const data = await ssg.get_top_threads.fetch({ num_threads: 10, period: 'day' })
   // console.log('state', ssg.dehydrate());
+
+  const rand = env.TWITTER_CLIENT_ID
+  const rand2 = env.TWITTER_CLIENT_SECRET
+
   return {
     props: {
       trpcState: ssg.dehydrate(),
-      threads: data.threads
+      threads: data.threads,
+      rand: rand,
+      rand2: rand2
     },
     revalidate: 600, // seconds
   };
 }
 
-export default function Home({ threads }: InferGetStaticPropsType<typeof getStaticProps>) {
-
+export default function Home({ threads, rand, rand2 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(rand, rand2)
   if (threads) {
     return (
       <>
