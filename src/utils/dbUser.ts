53e5188be2b866fdcb41d1647f2bf57b2de81da4
Harsh Patel <harshp2015m@gmail.com>
2023-01-26 00:17:31 -0500

    added save thread function

--- src/utils/dbUser.ts
+++ src/utils/dbUser.ts
@@ -14,6 +14,9 @@ export async function create_list(
     user_id: string,
     name: string
 ) {
+    /*
+    Creates a new list for a user with the specified name.
+    */
     const list = await prisma.list.create({
         data: {
             user_id: user_id,
@@ -22,4 +25,29 @@ export async function create_list(
     })
 
     return list
+}
+
+export async function save_thread(
+    thread_id: bigint | string,
+    user_id?: string,
+    list_id?: number
+) {
+    /*
+    Saves a thread to a list. If a list is not provided, one will be created for the given user.
+    */
+    if (!list_id && user_id) {
+        // default list name to Saved Threads
+        list_id = await create_list(user_id, "Saved Threads").then((list) => list.id)
+    }
+
+    let saved_thread;
+    if (list_id) {
+        saved_thread = await prisma.saved_thread.create({
+            data: {
+                list_id: list_id,
+                thread_id: BigInt(thread_id)
+            }
+        })
+    }
+    return saved_thread
 }
