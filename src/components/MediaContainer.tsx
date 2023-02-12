315e3ddf432f4901f57661f354ced254653fcd13
Harsh Patel <harshp2015m@gmail.com>
2023-02-11 22:59:23 -0500

    fixed gif rendering

--- src/components/MediaContainer.tsx
+++ src/components/MediaContainer.tsx
@@ -16,7 +16,7 @@ export default function MediaContainer({ media }: { media: media[] | undefined }
     // these are required for Lightbox component
     const [open, setOpen] = useState(-1) // contains the index of image to open
     const slides = media?.map((media_component) => {
-        if (media_component.type == 'video') {
+        if (media_component.type == 'video' || media_component.type == 'gif') {
             return ({
                 type: media_component.type,
                 width: 1280,
@@ -32,24 +32,28 @@ export default function MediaContainer({ media }: { media: media[] | undefined }
             return ({ src: media_component.url, type: media_component.type })
         }
     })
-
     return (
         <>
             <div className="grid grid-flow-col place-items-center bg-gray-200 gap-1 gap mt-2 w-full max-h-full rounded-3xl overflow-hidden border-4 border-gray-200">
                 {media &&
                     media.map((media_component, index) => {
+                        const is_gif = media_component.type == 'gif'
+
                         if (media_component.url) {
                             return (<div key={media_component.id}>
-                                {media_component.type == 'video' ?
+                                {media_component.type == 'video' || media_component.type == 'gif' ?
                                     <>
                                         <div className="video relative">
                                             <video
                                                 className="relative"
-                                                width="300"
-                                                height="300"
-                                                controls
+                                                width={is_gif ? "500" : "500"}
+                                                height={is_gif ? "500" : "500"}
                                                 disablePictureInPicture
-                                                muted>
+                                                muted
+                                                controls={!is_gif}
+                                                autoPlay={is_gif}
+                                                loop={is_gif}
+                                            >
                                                 <source src={media_component.url}
                                                     type="application/x-mpegURL" />
                                                 <source src={media_component.url}
